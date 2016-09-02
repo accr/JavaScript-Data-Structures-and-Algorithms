2b495b608dbc69a9cd1b6f385414cfccba76d848
accr <574150795@qq.com>
2016-09-02 16:36:24 +0800

    9.2

--- Dictionary.js
+++ Dictionary.js
@@ -118,4 +118,39 @@ console.log(hash.get('Loiane'));
 hash.remove('Gandalf');
 console.log(hash.get('Gandalf'));
 
+// 处理散列表中的冲突
+var hash = new HashTable();
+hash.put('Gandalf','gandalf@email.com');
+hash.put('John','johnsnow@email.com');
+hash.put('Tyrion','tyrion@email.com');
+hash.put('Aaron','aaron@email.com');
+hash.put('Donnie','donnie@email.com');
+hash.put('Ana','ana@email.com');
+hash.put('Jonathan','jonathan@email.com');
+hash.put('Jamie','jamie@email.com');
+hash.put('Sue','sue@email.com');
+hash.put('Mindy','mindy@email.com');
+hash.put('paul','paul@email.com');
+hash.put('Nathan','nathan@email.com');
+
+this.print = function(){
+    for(var i=0;i<table.length;++i){
+        if(table[i] !== undefined){
+            console.log(i+":"+table[i]);
+        }
+    }
+};
+
+hash.print();
+
+// 处理冲突：分离链接
+var ValuePair = function(key,value){
+    this.key = key;
+    this.value = value;
+
+    this.toString = function(){
+        return '[' + this.key + '-' + this.value +']';
+    }
+};
+
 
